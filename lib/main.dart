5c49c3be5ac5add600981575e86465ae1f8434b5
Weslei Tiago <weslei.tiago123@gmail.com>
2022-07-18 22:37:34 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uber/telas/Home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Uber",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
