469fa24728a530efa898770675397015483fef71
Weslei Tiago <weslei.tiago123@gmail.com>
2022-08-07 17:59:04 -0300

    implementing login and logout

--- lib/telas/Cadastro.dart
+++ lib/telas/Cadastro.dart
@@ -11,90 +11,77 @@ class Cadastro extends StatefulWidget {
 }
 
 class _CadastroState extends State<Cadastro> {
-
-  TextEditingController _controllerNome = TextEditingController(text: "weslei motorista");
-  TextEditingController _controllerEmail = TextEditingController(text: "weslei.motorista@gmail.com");
-  TextEditingController _controllerSenha = TextEditingController(text: "12345678");
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
   bool _tipoUsuario = false;
   String _mensagemErro = "";
 
-  _validarCampos(){
+  _validarCampos() {
     //recuperar dados dos campos
     String nome = _controllerNome.text;
     String email = _controllerEmail.text;
     String senha = _controllerSenha.text;
 
     //validar campos
-    if( nome.isNotEmpty){
-
-      if( email.isNotEmpty && email.contains("@")){
-
-        if(senha.isNotEmpty && senha.length > 6){
-
+    if (nome.isNotEmpty) {
+      if (email.isNotEmpty && email.contains("@")) {
+        if (senha.isNotEmpty && senha.length > 6) {
           Usuario usuario = Usuario();
           usuario.nome = nome;
           usuario.email = email;
           usuario.senha = senha;
           usuario.tipoUsuario = usuario.verificaTipoUsuario(_tipoUsuario);
 
-          _cadastrarUsuario( usuario );
-
-        }else{
+          _cadastrarUsuario(usuario);
+        } else {
           setState(() {
             _mensagemErro = "Preencha senha! digite mais de 6 caracteres";
           });
         }
-
-      }else{
+      } else {
         setState(() {
           _mensagemErro = "Preencha um Email válido";
         });
       }
-
-    }else{
+    } else {
       setState(() {
         _mensagemErro = "Preencha o Nome";
       });
     }
-
   }
 
-  _cadastrarUsuario(Usuario usuario){
-
+  _cadastrarUsuario(Usuario usuario) {
     FirebaseAuth auth = FirebaseAuth.instance;
     FirebaseFirestore db = FirebaseFirestore.instance;
-    
-    auth.createUserWithEmailAndPassword(
-        email: usuario.email!,
-        password: usuario.senha!,
-    ).then((firebaseUser) {
 
-      db.collection("usurios")
-          .doc( firebaseUser.user?.uid)
+    auth
+        .createUserWithEmailAndPassword(
+      email: usuario.email!,
+      password: usuario.senha!,
+    )
+        .then((firebaseUser) {
+      db
+          .collection("usuarios")
+          .doc(firebaseUser.user?.uid)
           .set(usuario.toMap());
 
       //redireciona para o painel, de acordo com o tipoUsuario
-      switch( usuario.tipoUsuario){
-        case "motorista" :
+      switch (usuario.tipoUsuario) {
+        case "motorista":
           Navigator.pushNamedAndRemoveUntil(
-              context,
-              "/painel-motorista",
-              (_) => false
-          );
+              context, "/painel-motorista", (_) => false);
           break;
-        case "passageiro" :
+        case "passageiro":
           Navigator.pushNamedAndRemoveUntil(
-              context,
-              "/painel-passageiro",
-                  (_) => false
-          );
+              context, "/painel-passageiro", (_) => false);
           break;
       }
-
+    }).catchError((error) {
+      _mensagemErro =
+          "Erro ao autenticar o usuário, verifique os campos e tente novamente!";
     });
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -121,8 +108,7 @@ class _CadastroState extends State<Cadastro> {
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(6),
-                      )
-                  ),
+                      )),
                 ),
                 TextField(
                   controller: _controllerEmail,
@@ -135,8 +121,7 @@ class _CadastroState extends State<Cadastro> {
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(6),
-                      )
-                  ),
+                      )),
                 ),
                 TextField(
                   controller: _controllerSenha,
@@ -150,25 +135,23 @@ class _CadastroState extends State<Cadastro> {
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(6),
-                      )
-                  ),
+                      )),
                 ),
                 Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Row(
-                      children: [
-                        Text("Passageiro"),
-                        Switch(
-                            value: _tipoUsuario,
-                            onChanged: (bool valor){
-                              setState(() {
-                                _tipoUsuario = valor;
-                              });
-                            }
-                        ),
-                        Text("Motorista"),
-                      ],
-                    ),
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Text("Passageiro"),
+                      Switch(
+                          value: _tipoUsuario,
+                          onChanged: (bool valor) {
+                            setState(() {
+                              _tipoUsuario = valor;
+                            });
+                          }),
+                      Text("Motorista"),
+                    ],
+                  ),
                 ),
                 Padding(
                     padding: EdgeInsets.only(top: 16, bottom: 10),
@@ -176,16 +159,14 @@ class _CadastroState extends State<Cadastro> {
                       style: ElevatedButton.styleFrom(
                           primary: Color(0xff1ebbd8),
                           padding: EdgeInsets.fromLTRB(32, 16, 32, 16)),
-
-                      onPressed: (){
-                          _validarCampos();
+                      onPressed: () {
+                        _validarCampos();
                       },
                       child: Text(
                         "Cadastrar",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
-                    )
-                ),
+                    )),
                 Padding(
                   padding: EdgeInsets.only(top: 16),
                   child: Center(
@@ -203,5 +184,3 @@ class _CadastroState extends State<Cadastro> {
     );
   }
 }
-
-
--- lib/telas/Home.dart
+++ lib/telas/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:uber/telas/Cadastro.dart';
+import '../model/Usuario.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,20 +11,110 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerEmail = TextEditingController(text: "weslei@gmail.com");
+  TextEditingController _controllerSenha = TextEditingController(text: "1234567");
+  String _mensagemErro = "";
+  bool _carregando = false;
 
-  TextEditingController _controllerEmail = TextEditingController();
-  TextEditingController _controllerSenha = TextEditingController();
+  _validarCampos() {
+    //recuperar dados dos campos
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    //validar campos
+    if (email.isNotEmpty && email.contains("@")) {
+      if (senha.isNotEmpty && senha.length > 6) {
+        Usuario usuario = Usuario();
+        usuario.email = email;
+        usuario.senha = senha;
+
+        _logarUsuario(usuario);
+      } else {
+        setState(() {
+          _mensagemErro = "Preencha senha! digite mais que 6 caracteres";
+        });
+      }
+    } else {
+      setState(() {
+        _mensagemErro = "Preencha um Email válido";
+      });
+    }
+  }
+
+  _logarUsuario(Usuario usuario) {
+
+    setState(() {
+      _carregando = true;
+    });
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    auth
+        .signInWithEmailAndPassword(
+          email: usuario.email!,
+          password: usuario.senha!
+        )
+        .then((firebaseUser) {
+      _redirecionaPainelPorTipoUsuario( firebaseUser.user!.uid );
+       })
+        .catchError((error) {
+      _mensagemErro =
+          "Erro ao autenticar o usuário, verifique e-mail e senha e tente novamente!";
+    });
+  }
+
+  _redirecionaPainelPorTipoUsuario(String idUsuario) async {
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    DocumentSnapshot snapshot = await db.collection("usuarios")
+    .doc( idUsuario)
+    .get();
+
+    dynamic dados = snapshot.data();
+
+    String tipoUsuario = dados["tipoUsuario"];
+    print("$dados");
+
+    setState(() {
+      _carregando = false;
+    });
+
+    switch( tipoUsuario ){
+      case "motorista":
+        Navigator.pushReplacementNamed(context, "/painel-motorista");
+        break;
+      case "passageiro":
+        Navigator.pushReplacementNamed(context, "/painel-passageiro");
+        break;
+
+    }
+  }
+
+  _verificaUsuarioLogado() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    User? usuarioLogado = await auth.currentUser;
+    if( usuarioLogado != null){
+      String idUsuario = usuarioLogado.uid;
+      _redirecionaPainelPorTipoUsuario(idUsuario);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _verificaUsuarioLogado();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("imagens/fundo.png"),
-              fit: BoxFit.cover
-          )
-        ),
+            image: DecorationImage(
+                image: AssetImage("imagens/fundo.png"), fit: BoxFit.cover)),
         padding: EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
@@ -30,12 +122,12 @@ class _HomeState extends State<Home> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Padding(
-                    padding: EdgeInsets.only(bottom: 32),
-                    child: Image.asset(
-                        "imagens/logo.png",
-                        width: 200,
-                      height: 150,
-                    ),
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "imagens/logo.png",
+                    width: 200,
+                    height: 150,
+                  ),
                 ),
                 TextField(
                   controller: _controllerEmail,
@@ -43,14 +135,13 @@ class _HomeState extends State<Home> {
                   keyboardType: TextInputType.emailAddress,
                   style: TextStyle(fontSize: 20),
                   decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "e-mail",
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                    )
-                  ),
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "e-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
                 ),
                 TextField(
                   controller: _controllerSenha,
@@ -64,40 +155,44 @@ class _HomeState extends State<Home> {
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(6),
-                      )
-                  ),
+                      )),
                 ),
                 Padding(
                     padding: EdgeInsets.only(top: 16, bottom: 10),
-                  child: ElevatedButton(
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           primary: Color(0xff1ebbd8),
                           padding: EdgeInsets.fromLTRB(32, 16, 32, 16)),
-
-                      onPressed: (){
-
+                      onPressed: () {
+                        _validarCampos();
                       },
                       child: Text(
-                          "Entrar",
+                        "Entrar",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
-                  )
-                ),
+                    )),
                 Center(
                   child: GestureDetector(
-                    child: Text("Não tem conta? cadastre-se!",
+                    child: Text(
+                      "Não tem conta? cadastre-se!",
                       style: TextStyle(color: Colors.white),
                     ),
-                    onTap: (){
+                    onTap: () {
                       Navigator.pushNamed(context, "/cadastro");
                     },
                   ),
                 ),
+                _carregando
+                    ? Padding(
+                    padding:EdgeInsets.only(top: 16),
+                    child: Center(
+                      child: CircularProgressIndicator(backgroundColor: Colors.white ,),))
+                    : Container(),
                 Padding(
                   padding: EdgeInsets.only(top: 16),
                   child: Center(
                     child: Text(
-                        "Erro",
+                      _mensagemErro,
                       style: TextStyle(color: Colors.red, fontSize: 20),
                     ),
                   ),
@@ -110,4 +205,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
